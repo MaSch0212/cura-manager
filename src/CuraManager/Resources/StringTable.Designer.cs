71fc546ee332806094e845824fd7033ee345bbe4
Marc Schmidt <masch212@gmail.com>
2021-12-19 22:28:11 +0100

    Correctly set Copyright information

--- src/CuraManager/Resources/StringTable.Designer.cs
+++ src/CuraManager/Resources/StringTable.Designer.cs
@@ -19,7 +19,7 @@ namespace CuraManager.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class StringTable {
@@ -138,15 +138,6 @@ namespace CuraManager.Resources {
         internal static string Close {
             get {
                 return ResourceManager.GetString("Close", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Copyright © 2019 by Marc Schmidt.
-        /// </summary>
-        internal static string Copyright {
-            get {
-                return ResourceManager.GetString("Copyright", resourceCulture);
             }
         }
         
--- src/CuraManager/ViewModels/Main/AboutViewModel.cs
+++ src/CuraManager/ViewModels/Main/AboutViewModel.cs
@@ -5,6 +5,7 @@ namespace CuraManager.ViewModels.Main;
 public class AboutViewModel : SplitViewContentViewModel
 {
     public static string CurrentVersion => App.GetCurrentVersion(true);
+    public static string Copyright => FileVersionInfo.GetVersionInfo(typeof(App).Assembly.Location).LegalCopyright;
 
     public AboutViewModel()
     {
